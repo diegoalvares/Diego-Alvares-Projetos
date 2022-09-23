3081f1b494712b89b65b21233cb2d7c18d590f80
Diego Alvares de Macedo <diegoalvares3@live.com>
2022-09-23 19:16:16 -0300

    Criando a tela de login
    Em construção.

---
+++ Projeto Cadastramento/tela_login.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from time import sleep
+
+tela_login = Tk()
+tela_login.geometry('600x400')
+tela_login.resizable(width=False, height=False)
+tela_login.configure(background='blue')
+
+
+tela_login.mainloop()
