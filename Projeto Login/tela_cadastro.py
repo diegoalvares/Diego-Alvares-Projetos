74495bfa4e7a148e6159526f967fdd323d38988c
Diego Alvares de Macedo <diegoalvares3@live.com>
2022-09-29 19:47:46 -0300

    Alteração na tela de login
    - Acrescentando os campos de e-mail e endereço.
    
    -Organização dos botões.
    
    - Inclusão de um título na tela,

--- Projeto Login/tela_cadastro.py
+++ Projeto Login/tela_cadastro.py
@@ -9,6 +9,8 @@ canvas1 = Canvas(tela_login, width=1000, height=800)
 canvas1.pack(fill="both", expand=True)
 canvas1.create_image(100, 280, image=bg)
 
+nome = canvas1.create_text(500, 50, text='Cadastro de Novo Cliente', fill='#FFFFFF', font=('Tahoma', 35, 'bold'))
+
 
 #------------------------------------ ENTRADAS ----------------------
 nome = canvas1.create_text(90, 120, text='Nome', fill='#FFFFFF', font=('Tahoma', 25, 'bold'))
@@ -19,30 +21,24 @@ tel = canvas1.create_text(110, 240, text='Telefone', fill='#FFFFFF', font=('Taho
 tel_entry = Entry(tela_login, font=('Tahoma', 20))
 tel_entry.place(x=40, y=270, height=35, width=550)
 
-end = canvas1.create_text(110, 240, text='Telefone', fill='#FFFFFF', font=('Tahoma', 25, 'bold'))
+email = canvas1.create_text(90, 360, text='E-mail', fill='#FFFFFF', font=('Tahoma', 25, 'bold'))
+email_entry = Entry(tela_login, font=('Tahoma', 20))
+email_entry.place(x=40, y=385, height=35, width=550)
+
+end = canvas1.create_text(115, 480, text='Endereço', fill='#FFFFFF', font=('Tahoma', 25, 'bold'))
 end_entry = Entry(tela_login, font=('Tahoma', 20))
-end_entry.place(x=40, y=270, height=35, width=550)
-
-
-
-#-------------- FUNÇÃO LOGIN ------------------------
-
-def login():
-    if user_entry.get().lower().strip() == user_name and password_entry.get() == password_login:
-        messagebox.showinfo(title='Aviso!', message='Login efetuado com sucesso!')
-    elif user_entry.get().lower().strip() != user_name or password_entry.get() != password_login:
-        messagebox.showerror(title='ERROR', message='Usuário ou senha incorreta!')
+end_entry.place(x=40, y=505, height=35, width=550)
 
 #--------------------- Botões -----------------------------
 
-bt1 = Button(tela_login, text='Salvar', font=('Tahoma', 15, 'bold'), background='#191970', foreground='#FFFFFF', command=login)
-bt1.place(x=120, y=400, width=90, height=35)
+bt1 = Button(tela_login, text='Salvar', font=('Tahoma', 18, 'bold'), background='#191970', foreground='#FFFFFF', command='')
+bt1.place(x=130, y=580, width=110, height=45)
 
-bt2 = Button(tela_login, text='Sair', font=('Tahoma', 15, 'bold'), background='#191970', foreground='#FFFFFF', command=tela_login.destroy)
-bt2.place(x=250, y=400, width=90, height=35)
+bt2 = Button(tela_login, text='Sair', font=('Tahoma', 18, 'bold'), background='#191970', foreground='#FFFFFF', command=tela_login.destroy)
+bt2.place(x=270, y=580, width=110, height=45)
 #--------------------- RODAPÉ -----------------------------
 
-rod = canvas1.create_text(225, 580, text='Empresas S/A', fill='#FFFFFF', font=('Tahoma', 15, 'bold'))
+rod = canvas1.create_text(240, 730, text='Empresas S/A', fill='#FFFFFF', font=('Tahoma', 15, 'bold'))
 
 
 tela_login.mainloop()
