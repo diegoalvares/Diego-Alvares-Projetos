6adae58f760a13e900f16d17a49f150824a5abfc
Diego Alvares de Macedo <diegoalvares3@live.com>
2022-10-24 18:58:49 -0300

    Tela Inicial
    - Criando a tela inicial do desenho.

---
+++ Projeto Desenhos/Super Mario.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+tela_Super_Mario = Tk()
+tela_Super_Mario.geometry('800x600')
+tela_Super_Mario.resizable(width=False, height=False)
+tela_Super_Mario.configure(bg = 'White')
+
+tela_Super_Mario.mainloop()
